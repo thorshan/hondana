5043191a87e4efec2799a694a854766f9801179a
unknown <myothunaing18@gmail.com>
2025-09-19 10:42:47 +0630

    Deployment

--- functions/index.js
+++
@@ -1,4 +0,0 @@
-const functions = require("firebase-functions");
-const express = require("express");
-const app = express();
-exports.api = functions.https.onRequest(app);
---
+++ netlify/functions/api.js
@@ -0,0 +1,49 @@
+// .env Configuration
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+
+
+
+
+const app = express();
+app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'public')));   
+
+
+// API calls
+const cors = require('cors');
+app.use(cors());
+
+// Middlewares
+app.use(express.urlencoded({ extended : true }));
+
+// Database connection
+const connectDb = require('./config/database');
+connectDb();
+
+// Routers Configurations
+const authorRoute = require('./routes/authorRoutes');
+const genreRoute = require('./routes/genresRoutes');
+const categoryRoute = require('./routes/categoryRoutes');
+const userRoute = require('./routes/userRoutes');
+const authRoute = require('./routes/authRoutes');
+
+// Author route
+app.use('/api', authorRoute);
+// Genre route
+app.use('/api', genreRoute);
+// Category route
+app.use('/api', categoryRoute);
+// User route
+app.use('/api', userRoute);
+// Authentication route
+app.use('/api', authRoute);
+
+// Netlify API Confug
+const serverless = require('serverless-http');
+module.exports.handler = serverless(app);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> console.log(`Server is running on http://localhost:${PORT}`));
